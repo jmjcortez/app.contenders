11e64927a71d7786c5f8ad18b2f8dde416e07435
Jared Cortez <jared.cortez@xref.com>
2019-12-20 08:53:45 +1100

    Recommend ui

--- src/js/components/homepage/recommendation/recommendation.component.jsx
+++ src/js/components/homepage/recommendation/recommendation.component.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import { CardDeck, Card } from 'react-bootstrap';
+import styled from 'styled-components';
 
 import RecommendedUser from './recommended-user';
 
+const RecommendedContainer = styled.div`
+    margin: 30px;    
+`;
+
 export const Recommendation = props => {
 
-    const { setNextRecommenedUser } = props;
+    const { setNextRecommenedUser, recommendationList } = props;
+
+    const recommendedUsers = recommendationList.map((recommendedUser) =>
+        <Card>
+            <Card.Img variant="top" src="https://moneydotcomvip.files.wordpress.com/2019/03/conor-mcgregor-net-worth-career-earnings-1046270310.jpg" />
+            <Card.Body>
+            <Card.Title>{ recommendedUser.first_name }</Card.Title>
+            <Card.Text>
+                { recommendedUser.bio_text }
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <small className="text-muted">Last seen 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+    );
 
     return (
-        <div>
-            <div>Recommendation</div>
-            <RecommendedUser />
-            <button onClick={setNextRecommenedUser}>Next</button>
-        </div>
+        <RecommendedContainer>
+            <CardDeck>
+                {recommendedUsers}
+            </CardDeck>
+        </RecommendedContainer>
   )
 };
 
